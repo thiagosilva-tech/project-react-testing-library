a317b06014eb1fa5e7f89aae29940acf4fa414f4
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-09-12 14:38:34 -0300

    feat: requisito 04

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se a página contém um heading h2 com o texto Page requested not found.', () => {
+  renderWithRouter(<App />, { route: '/123' });
+
+  const textNotFound = screen.getByRole('heading', { level: 2 });
+  expect(textNotFound.textContent).toBe('Page requested not found');
+});
+
+test('Teste se a página mostra a imagem com o texto alternativo.', () => {
+  renderWithRouter(<App />, { route: '/123' });
+
+  const image = screen.getByAltText(/Clefairy pushing buttons randomly with text I have no idea what i'm doing/i);
+
+  expect(image).toBeInTheDocument();
+});
