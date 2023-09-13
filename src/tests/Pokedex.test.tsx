5da7684e810050b0495bac9c911878dc91d6851f
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-09-13 20:56:48 -0300

    fix: eslint

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
